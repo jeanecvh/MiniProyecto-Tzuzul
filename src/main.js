00628482a6dd9f2faec6de73e1dc5e8e46b7ed2e
jeanellach <jeanellach@gmail.com>
2021-12-11 21:31:00 -0500

    orden

---
+++ src/main.js
@@ -0,0 +1,46 @@
+let dataCharmander =document.getElementById("data_charmander");
+let dataTotodile = document.getElementById("data_totodile");
+let live = "VIDA: "
+let liveCharmander = "100";
+let liveTotodile = "100";
+let attackButtonCharmander = document.getElementById("attack-button-charmander");
+let attackButtonTotodile = document.getElementById("attack-button-totodile");
+
+class Pokemon {
+    constructor (vida,tamaño,ataque,puntajeAtaque) {
+        this.vida = vida
+        this.tamaño = tamaño
+        this.ataque = ataque
+        this.puntajeAtaque = puntajeAtaque
+    }        
+}
+
+let charmander = new Pokemon (100, "0,6 m", "LAZA LLAMAS",20);
+    console.log(charmander)
+let totodile = new Pokemon (100, "0,6 m", "TORRENTE",25);
+    console.log(totodile)
+
+    let divCharmander = document.createElement("div");
+    divCharmander.className = "description"
+    let charmanderTemplete = `
+    <p>VIDA: ${charmander.vida}</p><br>
+    <p>TAMAÑO: ${charmander.tamaño}</p><br>
+    <p>TAMAÑO: ${charmander.ataque}</p><br>
+    `
+    divCharmander.innerHTML = charmanderTemplete
+    dataCharmander.appendChild(divCharmander);
+
+    let divTotodile = document.createElement("div");
+    divTotodile.className = "description"
+    let totodileTemplete = `
+    <p>VIDA: ${totodile.vida}</p><br>
+    <p>TAMAÑO: ${totodile.tamaño}</p><br>
+    <p>TAMAÑO: ${totodile.ataque}</p><br>
+    `
+    divTotodile.innerHTML = totodileTemplete
+    dataTotodile.appendChild(divTotodile);
+
+attackButtonCharmander.onclick = function (){
+    
+}
+
