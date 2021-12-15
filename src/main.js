392ad2595bd470f42d0503851d1e6296830fc71d
jeanellach <jeanellach@gmail.com>
2021-12-15 12:07:59 -0500

    function attack

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ let dataCharmander =document.getElementById("data_charmander");
 let dataTotodile = document.getElementById("data_totodile");
 let attackButtonCharmander = document.getElementById("attack-button-charmander");
 let attackButtonTotodile = document.getElementById("attack-button-totodile");
+let attackName = '';
+
 
 class Pokemon {
     constructor (vida,tamaño,ataque,puntajeAtaque) {
@@ -17,30 +19,46 @@ let charmander = new Pokemon (100, "0,6 m", "LAZA LLAMAS",20);
 let totodile = new Pokemon (100, "0,6 m", "TORRENTE",25);
     console.log(totodile)
 
-    let divCharmander = document.createElement("div");
-    divCharmander.className = "description"
-    let charmanderTemplete = `
-    <p>VIDA: ${charmander.vida}</p><br>
-    <p>TAMAÑO: ${charmander.tamaño}</p><br>
-    <p>TAMAÑO: ${charmander.ataque}</p><br>
-    `
-    divCharmander.innerHTML = charmanderTemplete
-    dataCharmander.appendChild(divCharmander);
-
-    let divTotodile = document.createElement("div");
-    divTotodile.className = "description"
-    let totodileTemplete = `
-    <p>VIDA: ${totodile.vida}</p><br>
-    <p>TAMAÑO: ${totodile.tamaño}</p><br>
-    <p>TAMAÑO: ${totodile.ataque}</p><br>
-    `
-    divTotodile.innerHTML = totodileTemplete
-    dataTotodile.appendChild(divTotodile);
-
-
-attackButtonCharmander.onclick = function (pokemonVida = 100){
-    vidaAcumulada
-    for (let i=0; i < pokemonVida; i--)
-
+attackButtonCharmander.onclick = function (){
+        totodile.vida = (totodile.vida - charmander.puntajeAtaque);{
+            let divCharmander = document.createElement("div");
+            divCharmander.className = "description"
+            let charmanderTemplete = `
+            <div id= "charmanderCaracteristics">
+                <div id= "charmanderLive">
+                    <p>VIDA: ${charmander.vida}</p><br>
+                </div>
+                    <p>TAMAÑO: ${charmander.tamaño}</p><br>
+                    <p>ATAQUE: ${charmander.ataque}</p><br>
+            </div>
+            `
+        divCharmander.innerHTML = charmanderTemplete
+        dataCharmander.appendChild(divCharmander);
+        }
 }
+
+attackButtonTotodile.onclick = function (){
+    charmander.vida = (charmander.vida - totodile.puntajeAtaque);{
+        let divTotodile = document.createElement("div");
+        divTotodile.className = "description"
+        let totodileTemplete = `
+            <div div id= "totodileCaracteristics">
+                <div id= "charmanderLive">
+                    <p>VIDA: ${totodile.vida}</p><br>
+                </div> 
+                    <p>TAMAÑO: ${totodile.tamaño}</p><br>
+                    <p>ATAQUE: ${totodile.ataque}</p><br>
+            </div> 
+        `
+        divTotodile.innerHTML = totodileTemplete
+        dataTotodile.appendChild(divTotodile);
+    }
+}
+
+    
+
+    
+
+
+
 
